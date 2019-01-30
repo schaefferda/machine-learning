edcb09e913b5aa4e7a71bcd035ae30cfcf02ba7b
Daniel Schaeffer <dschaeffer@harmonywave.cloud>
2019-01-30 15:16:20 -0700

    Lecture walkthrough

--- Part2-Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Part2-Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -21,7 +21,7 @@ X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -39,3 +39,24 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# Build the optimal model using backward elimination
+import statsmodels.formula.api as sm
+X = np.append(np.ones((50,1)).astype(int), X, axis=1)
+
+# Backward elminiation with only P-values
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
